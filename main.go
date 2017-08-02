f4b446b49231856e745cff8923075c4f9c1da136
gatchi <christen.got@gmail.com>
2017-08-01 17:47:52 -0700

    added custom ScanWords implementation
    this scan words also scans and strips commas cause why not really.

---
+++ main.go
@@ -0,0 +1,41 @@
+package utilgo
+
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+/**
+ * ScanWords is a split function for a Scanner that returns each
+ * space-separated word of text, with surrounding spaces deleted. It will
+ * never return an empty string. The definition of space is set by
+ * unicode.IsSpace.
+ */
+func ScanWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if !unicode.IsSpace(r) {
+			break
+		}
+	}
+
+	// Scan until space or comma, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if unicode.IsSpace(r) || r == ',' {
+			return i + width, data[start:i], nil
+		}
+	}
+
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+
+	// Request more data.
+	return start, nil, nil
+}
